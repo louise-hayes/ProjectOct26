e274876d987eac2e143f79e56ff2dce0515e8189
Louise Hayes <louise@deewhy.ie>
2017-10-26 20:24:20 -0400

    showing rows

--- main.js
+++ main.js
@@ -21,11 +21,11 @@ var start = "";
 var rate = "";
 
 // Setup
-$(document).ready(function() {
+$(document).ready(function () {
 
 
     // user clicked in the login area
-    $('body').on('click', '#add-user', function(event) {
+    $('body').on('click', '#add-user', function (event) {
         // console.log('player clicked: ' + player);
         event.preventDefault();
         console.log('adding user....');
@@ -77,32 +77,56 @@ database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", functi
 
 
 // Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
-database.ref().on("child_added", function(childSnapshot) {
-    
-          // Log everything that's coming out of snapshot
-          console.log(childSnapshot.val().name);
-          console.log(childSnapshot.val().role);
-          console.log(childSnapshot.val().start);
-          console.log(childSnapshot.val().rate);
-        
-    
-        //   // full list of items to the well
-        //   $("#full-member-list").append("<div class='well'><span id='name'> " + childSnapshot.val().name +
-        //     " </span><span id='email'> " + childSnapshot.val().email +
-        //     " </span><span id='age'> " + childSnapshot.val().age +
-        //     " </span><span id='comment'> " + childSnapshot.val().comment + " </span></div>");
-    
-        // Handle the errors
-        }, function(errorObject) {
-          console.log("Errors handled: " + errorObject.code);
-        });
-    
-        // dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
-    
-        //   // Change the HTML to reflect
-        //   $("#name-display").text(snapshot.val().name);
-        //   $("#email-display").text(snapshot.val().email);
-        //   $("#age-display").text(snapshot.val().age);
-        //   $("#comment-display").text(snapshot.val().comment);
-        // });
-    
+database.ref().on("child_added", function (childSnapshot) {
+
+    // Log everything that's coming out of snapshot
+    console.log("childSnapshot.val().name" + childSnapshot.val().name);
+    console.log(childSnapshot.val().role);
+    console.log(childSnapshot.val().start);
+    console.log(childSnapshot.val().rate);
+
+    var name = childSnapshot.val().name;
+    var role = childSnapshot.val().role;
+    var start = (childSnapshot.val().start);
+    var rate = (childSnapshot.val().rate);
+
+    // full list of items to the well
+    var varDiv = $('<tr>')
+    var td1 = $('<td>' + name + '</td>');
+    var td2 = $('<td>' + role + '</td>');
+    var td3 = $('<td>' + start + '</td>');
+    var td4 = $('<td>' + '</td>');
+    var td5 = $('<td>' + rate + '</td>');
+    var td6 = $('<td>' + '</td>');
+
+
+    varDiv.append(td1);
+    varDiv.append(td2);
+    varDiv.append(td3);
+    varDiv.append(td4);
+    varDiv.append(td5);
+    varDiv.append(td6);
+
+    $("#tableBody").append(varDiv);
+
+
+
+
+    // $("#tableBody").append(childSnapshot.val().role);
+    //     " </span><span id='email'> " + childSnapshot.val().email +
+    //     " </span><span id='age'> " + childSnapshot.val().age +
+    //     " </span><span id='comment'> " + childSnapshot.val().comment + " </span></div>");
+
+    // Handle the errors
+}, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
+
+// dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+
+//   // Change the HTML to reflect
+//   $("#name-display").text(snapshot.val().name);
+//   $("#email-display").text(snapshot.val().email);
+//   $("#age-display").text(snapshot.val().age);
+//   $("#comment-display").text(snapshot.val().comment);
+// });
