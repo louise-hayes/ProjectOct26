6bb83739f28ba02e21f5c40e67f994953282513d
Michael Fessenden <michael.fessenden@gmail.com>
2017-10-26 21:04:19 -0400

    add date diff

--- main.js
+++ main.js
@@ -42,23 +42,20 @@ $(document).ready(function() {
 // Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
 database.ref().on("child_added", function (childSnapshot) {
 
-    // Log everything that's coming out of snapshot
-    console.log("childSnapshot.val().name" + childSnapshot.val().name);
-    console.log(childSnapshot.val().role);
-    console.log(childSnapshot.val().start);
-    console.log(childSnapshot.val().rate);
-
     var name = childSnapshot.val().name;
     var role = childSnapshot.val().role;
     var start = (childSnapshot.val().start);
     var rate = (childSnapshot.val().rate);
+
+    var monthsSinceStart = moment().diff(start, "months");
+    console.log(monthsSinceStart);
 
     // full list of items to the well
     var varDiv = $('<tr>')
     var td1 = $('<td>' + name + '</td>');
     var td2 = $('<td>' + role + '</td>');
     var td3 = $('<td>' + start + '</td>');
-    var td4 = $('<td>' + '</td>');
+    var td4 = $('<td>' + monthsSinceStart + '</td>');
     var td5 = $('<td>' + rate + '</td>');
     var td6 = $('<td>' + '</td>');
 
